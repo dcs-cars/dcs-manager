0236125070a410f75fccbb29a47c9bfcc51f9dbf
Patrik Stutz <patrik.stutz@gmail.com>
2017-01-05 18:23:01 +0100

    added authentication

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ var Koa = require("koa");
 var mount = require("koa-mount");
 var mongoose = require("mongoose");
 var files = require("koa-static");
+var auth = require("koa-basic-auth");
 var fs = require("fs");
+
+var config = JSON.parse(fs.readFileSync("./config.json")+"");
 
 require("babel-polyfill");
 
@@ -10,6 +13,21 @@ var con = mongoose.connect("mongodb://localhost/dcs");
 require("./models")(con);
 
 var app = new Koa();
+app.use(async (ctx, next) => {
+	try {
+		await next();
+	} catch (err) {
+		if (401 == err.status) {
+			ctx.status = 401;
+			ctx.set('WWW-Authenticate', 'Basic');
+		} else {
+			throw err;
+		}
+	}
+});
+
+// require auth
+app.use(auth({ name: 'dcs', pass: config.password }));
 app.use(mount("/public",files("./public")));
 app.use(async function(ctx,next){
 	ctx.db = con;
@@ -20,4 +38,4 @@ app.use(async function(c){
 	c.set("Content-Type","text/html");
 	c.body = "<html><head><link rel='stylesheet' href='/public/bootstrap/css/bootstrap.min.css'/><link rel='stylesheet' href='/public/react-widgets/css/react-widgets.css'/><script src='/public/main.js'></script></head><body></body></html>";
 })
-app.listen(80);
+app.listen(config.port);
